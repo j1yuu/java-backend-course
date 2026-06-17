cf1d7a1f553989e4f5f894714d7e9e4b2715bfd5
Konstantin <o.lover4@bk.ru>
2026-06-18 02:44:02 +0300

    lesson: get/post/put/delete

---
+++ spring-boot-web/lessons/src/main/java/kkashin/dev/lessons/controller/BookController.java
@@ -0,0 +1,62 @@
+package kkashin.dev.lessons.controller;
+
+import kkashin.dev.lessons.model.Book;
+import kkashin.dev.lessons.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks(
+            @RequestParam(value = "authorName", required = false) String authorName,
+            @RequestParam(value = "maxCost", required = false) Integer maxCost
+    ) {
+        log.info("Get req for getAllBooks");
+        return bookService.searchAllBooks(authorName, maxCost);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        log.info("Get req for getBookById: {}", id);
+        return bookService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book bookToCreate) {
+        log.info("Post req for bookToCreate: {}", bookToCreate);
+        var book = bookService.createBook(bookToCreate);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        log.info("Delete req for deleteById: {}", id);
+        bookService.deleteById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        log.info("Update req for updateBook: {}", book);
+        return bookService.updateBook(id, book);
+    }
+}
--- spring-boot-web/lessons/src/main/java/kkashin/dev/lessons/model/Book.java
+++ spring-boot-web/lessons/src/main/java/kkashin/dev/lessons/model/Book.java
@@ -1,11 +1,22 @@
 package kkashin.dev.lessons.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record Book(
         Long id,
         String name,
+
+        @JsonProperty("author_name")
         String authorName,
+
+        @JsonProperty("publication_year")
         Integer publicationYear,
+
+        @JsonProperty("page_number")
         Integer pageNumber,
+
         Integer cost
 ) {
 }
