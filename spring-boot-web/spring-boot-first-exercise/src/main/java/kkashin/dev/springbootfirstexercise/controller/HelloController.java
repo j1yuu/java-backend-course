03ea313b054094cf27766afdc5383311e35f7527
Konstantin <o.lover4@bk.ru>
2026-06-18 01:21:45 +0300

    feat: completed hw

---
+++ spring-boot-web/spring-boot-first-exercise/src/main/java/kkashin/dev/springbootfirstexercise/controller/HelloController.java
@@ -0,0 +1,13 @@
+package kkashin.dev.springbootfirstexercise.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Boot is running!";
+    }
+}
