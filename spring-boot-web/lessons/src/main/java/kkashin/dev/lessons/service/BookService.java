05b8871d5234832ba28b409054698e92c1722b9d
Konstantin <59069015+j1yuu@users.noreply.github.com>
2026-06-22 00:26:29 +0300

    [SPRING BOOT WEB] Lessons
    * lesson: first project
    
    * lesson: get/post/put/delete
    
    * lessons: testing

---
+++ spring-boot-web/lessons/src/main/java/kkashin/dev/lessons/service/BookService.java
@@ -0,0 +1,98 @@
+package kkashin.dev.lessons.service;
+
+import kkashin.dev.lessons.model.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class BookService {
+    private final AtomicLong idCounter;
+    private final Map<Long, Book> bookMap;
+
+    public BookService() {
+        this.idCounter = new AtomicLong(0);
+        this.bookMap = new HashMap<>();
+
+        createBook(new Book(
+                null,
+                "book 1",
+                "author",
+                2024,
+                100,
+                1000
+        ));
+        createBook(new Book(
+                null,
+                "book 2",
+                "author",
+                2025,
+                200,
+                1000
+        ));
+        createBook(new Book(
+                null,
+                "book 3",
+                "author 2",
+                2022,
+                100,
+                500
+        ));
+    }
+
+    public List<Book> searchAllBooks(String authorName, Integer maxCost) {
+        return bookMap.values().stream()
+                .filter(b -> authorName.isEmpty() || b.authorName().contains(authorName))
+                .filter(b -> maxCost == null || b.cost() < maxCost)
+                .toList();
+    }
+
+    public Book findById(Long id) {
+        var book = bookMap.get(id);
+
+        if (book == null) throw new NoSuchElementException("Book not found");
+        return book;
+    }
+
+    public Book createBook(Book bookToCreate) {
+        var newId = idCounter.getAndIncrement();
+
+        var newBook = new Book(
+                newId,
+                bookToCreate.name(),
+                bookToCreate.authorName(),
+                bookToCreate.publicationYear(),
+                bookToCreate.pageNumber(),
+                bookToCreate.cost()
+        );
+
+        bookMap.put(newId, newBook);
+
+        return newBook;
+    }
+
+    public void deleteById(Long id) {
+        if (bookMap.get(id) == null) throw new NoSuchElementException("Book not found");
+
+        bookMap.remove(id);
+    }
+
+    public Book updateBook(Long id, Book book) {
+        if (bookMap.get(id) == null) throw new NoSuchElementException("Book not found");
+
+        var updated = new Book(
+                id,
+                book.name(),
+                book.authorName(),
+                book.publicationYear(),
+                book.pageNumber(),
+                book.cost()
+        );
+
+        bookMap.put(id, updated);
+
+        return updated;
+    }
+
+}
